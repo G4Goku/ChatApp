02fbf5670e56477178f7e037c18b2c306f8aafd6
G4Goku <g4gokulsuresh@gmail.com>
2024-07-22 22:20:55 +0530

    initial commits

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,26 @@
-// require('dotenv').config()
-// const mongoose = require('mongoose')
-// const express = require('express')
-// const cors = require('cors')
-// const morgan =require('morgan')
-// const app = express()
-// const user = require('./routes/auth.route')
-// const mongoUrl = process.env.MONGO_URL
+require('dotenv').config()
+import mongoose from 'mongoose'
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import http from 'http'
+// import user from './routes/auth.route'
+const app = express()
+const mongoUrl = process.env.MONGO_URL
+const server = http.createServer(app)
 
-// if(! process.env.JWT_TOKEN) {
-//     console.error("FATAL ERROR : jwt private key not defined");
-//     process.exit(1)
-// }
-// mongoose.connect(mongoUrl)
-//     .then(() => console.log("Mongo db connected"))
-//     .catch((err) => console.log("Mongo db connection failed",err.message) )
+if (!process.env.JWT_TOKEN) {
+    console.error("FATAL ERROR : jwt private key not defined");
+    process.exit(1)
+}
+mongoose.connect(mongoUrl || "")
+    .then(() => console.log("Mongo db connected"))
+    .catch((err: any) => console.log("Mongo db connection failed", err.message))
 
-// app.use(cors())
-// app.use(express.json())
-// app.use(morgan('tiny'))
-// app.use('/api/v1/vidly/user',user)
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+// app.use('/api/v1/vidly/user', user)
 
-// const port = process.env.PORT || 5000
-// app.listen(port, () => console.log(`App listening to port ${port}`))
-
-console.log("ts")
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`App listening to port ${port}`))
---
+++ server/src/socket/socket.ts
@@ -0,0 +1,5 @@
+export default class HandleSocket {
+    static async InitiateSocket() {
+
+    }
+}
